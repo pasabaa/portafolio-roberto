49b6f34405d4a4edf331f1bb837a17cadd866970
pasabaa <sanchez.ballesteros.pablo@gmail.com>
2023-02-28 11:53:07 -0600

    delete some characters

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -6,7 +6,7 @@ export const Hero = () => {
       <img className='relative object-cover w-full h-full rounded-xl' src="/static/images/hero.webp" alt="" />
       <div className='absolute inset-0 p-8 xl:pb-16 bg-gradient-to-t from-[#ef6d58] to-gray-300/10 rounded-xl flex items-end'>
         <div>
-          <h1 className='text-5xl xl:text-6xl font-semibold max-w-xs'>We're Help To Build Your Dream Project</h1>
+          <h1 className='text-5xl xl:text-6xl font-semibold max-w-xs'>Were Help To Build Your Dream Project</h1>
           <p className='max-w-xs text-gray-300 mt-4 hidden xl:block'>Agency provides a full service range including technical skills, design, business understading.</p>
           <div className='flex gap-4 mt-4'>
             <button className='uppercase px-4 py-3 bg-[#ef6d58] rounded-xl'>Nuestro trabajo</button>
